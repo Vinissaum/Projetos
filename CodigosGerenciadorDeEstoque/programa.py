7c558521846ad0be866ba29efb20cd69fb7e9118
Vinissaum <74688433+Vinissaum@users.noreply.github.com>
2021-04-12 17:01:20 -0300

    Update programa.py

--- CodigosGerenciadorDeEstoque/programa.py
+++ CodigosGerenciadorDeEstoque/programa.py
@@ -1,9 +1,21 @@
 import gerenciar.bd as bd
 
 def adicionarItem():
-    item = str(input('Insira o nome do item que deseja adicionar: '))
-    quantidade = int(input('Insira a quantidade disponivel desse item: '))
-    bd.inserirItens(item, quantidade)
+    condition = True
+    while condition:
+        item = str(input('Insira o nome do item que deseja adicionar: '))
+        quantidade = int(input('Insira a quantidade disponivel desse item: '))
+        bd.inserirItens(item, quantidade)
+        while True:
+            resposta = str(input('Gostaria de adicionar mais um item?: ')).lower()
+            if(resposta == 's' or resposta == 'sim'):
+                condition = True
+                break
+            elif(resposta == 'n' or resposta == 'nao' or resposta == 'não'):
+                condition = False
+                break
+            else:
+                print('Escreva se sim ou não!')
 
 def alterarItem():
     id_item = int(input('Digite o id do item que deseja alterar: '))
@@ -16,28 +28,56 @@ def alterarQtd():
     bd.alterarQuantidade(quantidade, id_item)
 
 def verItens():
-    for dados in bd.lerItens():
-        print('Id: {:<20} Item: {:^20} Quantidade: {:>20}'.format(dados[0], dados[1],dados[2]))
+    item = '[ITENS]'
+    quantidade = '[QUANTIDADE]'
+    print('{:^35} {}'.format(item, quantidade))
+    for dados in bd.lerItens():      
+        print('[Id: {}] {:^20} {:>14}'.format(dados[0], dados[1],dados[2]))
 
 def subtrairItem():
-    idcomparar = int(input('Digite o Id do item que foi usado: '))
-    for dado in bd.lerItens():
-        if(idcomparar == dado[0]):
-            subtrai = int(input('Quantos itens foram utilizados?: '))
-            quantidade = int(dado[2])
-            quantidade = quantidade - subtrai
-            id_item = idcomparar
-            bd.alterarQuantidade(quantidade, id_item)
+    condition = True
+    while condition:
+        idcomparar = int(input('Digite o Id do item que foi usado: '))
+        for dado in bd.lerItens():
+            if(idcomparar == dado[0]):
+                subtrai = int(input('Quantos itens foram utilizados?: '))
+                quantidade = int(dado[2])
+                quantidade = quantidade - subtrai
+                id_item = idcomparar
+                bd.alterarQuantidade(quantidade, id_item)
+        while True:
+            resposta = str(input('Gostaria de subtrair de mais um item?: ')).lower()
+            if(resposta == 's' or resposta == 'sim'):
+                condition = True
+                break
+            elif(resposta == 'n' or resposta == 'nao' or resposta == 'não'):
+                condition = False
+                break
+            else:
+                print('Escreva se sim ou não!')
+        
 
 def somaItem():
-    idcomparar = int(input('Digite o Id do item que foi reabastecido: '))
-    for dado in bd.lerItens():
-        if(idcomparar == dado[0]):
-            somar = int(input('Quantos itens foram reabastecidos?: '))
-            quantidade = int(dado[2])
-            quantidade = quantidade + somar
-            id_item = idcomparar
-            bd.alterarQuantidade(quantidade, id_item)
+    condition = True
+    while condition:
+        idcomparar = int(input('Digite o Id do item que foi reabastecido: '))
+        for dado in bd.lerItens():
+            if(idcomparar == dado[0]):
+                somar = int(input('Quantos itens foram reabastecidos?: '))
+                quantidade = int(dado[2])
+                quantidade = quantidade + somar
+                id_item = idcomparar
+                bd.alterarQuantidade(quantidade, id_item)
+        while True:
+            resposta = str(input('Gostaria de adicionar a mais um item?: ')).lower()
+            if(resposta == 's' or resposta == 'sim'):
+                condition = True
+                break
+            elif(resposta == 'n' or resposta == 'nao' or resposta == 'não'):
+                condition = False
+                break
+            else:
+                print('Escreva se sim ou não!')
 
 def excluir():
     condition = True
